f0c6248c4e05f779c86e05b328659890c0e574ac
codyman <ijuyeong@ijuyeong-ui-MacBookPro.local>
2023-11-26 11:48:05 +0900

    fallmotion

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,8 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import Hero from "@/components/marketing/features/hero";
+import { FallInPlace } from "@/components/motion/fall-in-place";
+import { Br } from "@/components/typography";
+import { Box, Stack } from "@chakra-ui/react";
+import Head from "next/head";
 
 export default function Home() {
   return (
@@ -14,101 +13,37 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
+      <Hero
+        as={Stack}
+        id="home"
+        alignItems="center"
+        textAlign="center"
+        title={
+          <FallInPlace textAlign="center" initialInView>
+            Codyman의 웹 개발 저장소를 공유합니다
+          </FallInPlace>
+        }
+        description={
+          <FallInPlace
+            delay={0.4}
+            fontWeight="medium"
+            textAlign="center"
+            color="gray.600"
+            _dark={{ color: "gray.400" }}
+            fontSize={{ base: "md", md: "xl", lg: "2xl" }}
+            initialInView
+            pt="12"
+            pb="12"
           >
-            <h2>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+            미국의 소스코드를 분석하여 모방하여 의존성을 제거하여 공유하기 위해
+            노력합니다.
+            <Br display={{ base: "none", md: "inline" }} /> Operate and manage
+            여러 작은 실험을 통해 확장해나가려고 합니다.{" "}
+            <Br display={{ base: "none", md: "inline" }} /> View analytics and
+            피드백과 많은 참여 부탁드립니다.
+          </FallInPlace>
+        }
+      ></Hero>
     </>
-  )
+  );
 }
